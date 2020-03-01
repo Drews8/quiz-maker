dc77acf47c90fb5739cb7a8086f082dc5ea86e55
Drews8 <prokhorenko.drew@gmail.com>
2020-03-01 12:37:45 +0200

    add click handler for aside

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,17 +1,20 @@
 import React, {Component} from "react";
 import classes from "./Drawer.module.css"
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 
-const links = [1, 2, 3]
+const links = [1, 2, 3];
+
 
 class Drawer extends Component {
+
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li
           key={index}
         >
-          Link {link}
+          <a>Link {link}</a>
         </li>
       )
     })
@@ -19,13 +22,22 @@ class Drawer extends Component {
   }
 
   render() {
+    const cls = [classes.Drawer];
+
+    if (!this.props.isOpen) {
+      cls.push(classes.close)
+    }
+
 
     return (
-      <nav className={classes.Drawer}>
-        <ul>
-          {this.renderLinks()}
-        </ul>
-      </nav>
+      <React.Fragment>
+        <nav className={cls.join(' ')}>
+          <ul>
+            {this.renderLinks()}
+          </ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null}
+      </React.Fragment>
     )
   }
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import classes from "./Layout.module.css"
 import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component{
   state = {
@@ -9,13 +10,21 @@ class Layout extends Component{
 
   toggleMenuHandler = () => {
     this.setState({
-      menu: !this.state.menu
+      menu: !this.state.menu,
+    })
+  }
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false,
     })
   }
     render() {
         return (
             <div className={classes.Layout}>
-
+                <Drawer
+                  isOpen={this.state.menu}
+                  onClose={this.menuCloseHandler}
+                />
                 <MenuToggle
                   onToggle={this.toggleMenuHandler}
                   isOpen={this.state.menu}
