2cc4a744a5b03696d38aa9180326ce56c775b129
Drews8 <prokhorenko.drew@gmail.com>
2020-03-08 23:09:15 +0200

    add nav routing

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,20 +1,34 @@
 import React, {Component} from "react";
 import classes from "./Drawer.module.css"
 import Backdrop from "../../UI/Backdrop/Backdrop";
+import {NavLink} from "react-router-dom";
 
 
-const links = [1, 2, 3];
+const links = [
+  {to: '/', label: 'Список', exact: true},
+  {to: '/auth', label: 'Авторизація', exact: false},
+  {to: '/quiz-creator', label: 'Створити тест', exact: false},
+];
 
 
 class Drawer extends Component {
 
+  clickHandler = () => {
+    this.props.onClose();
+  }
+
   renderLinks() {
     return links.map((link, index) => {
       return (
-        <li
-          key={index}
-        >
-          <a>Link {link}</a>
+        <li key={index}>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
         </li>
       )
     })
